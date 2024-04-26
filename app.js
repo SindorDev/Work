f297f4e2a6c2d3e7345889ab3af7a086a7f7a470
Sindor007 <thesso003@gmail.com>
2024-04-26 09:12:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,156 @@
+// let m = prompt("kilogrammni kiriting");
+// let t = m / 1000;
+
+// console.log(t)
+
+
+// let f = prompt("faylni hajmini kiriting");
+// let t = f * 1024;
+
+// console.log(t + " " + "baytga teng");
+
+// let a = Number(prompt("musbat son kiriting"));
+// let b = Number(prompt("manfiy son kiriting"))
+
+// let result = a / b;
+
+// console.log(result)
+
+// let a = Number(prompt("musbat son kiriting"));
+// let b = Number(prompt("manfiy son kiriting"))
+
+
+// let result = parseInt(a / b);
+// let qoldiq = a % b
+
+// console.log(
+//      a +  " ni ichida " +
+//      b + " ni " +
+//      result + 
+//      "marta joylashtirsa boladi. Qoldiq:" + 
+//      qoldiq
+// )
+
+// let UserName = prompt("Ismingizni kiriting ");
+
+// if(UserName == "John") {
+//      console.log("Admin ")
+// }
+// else {
+//      console.log("user")
+// }
+
+// let UserName = prompt("Yoshingizni kiriting ");
+
+// if(UserName <= 15) {
+//      console.log("15% Chegirma")
+// }
+// else {
+//      console.log("Odammassiz Shuning Uchun Chegirma Yoq")
+// }
+
+
+// let UserName = prompt("Son kiriting");
+
+// if(UserName % 2 == 0 ) {
+//      console.log("Juft")
+// }
+// else {
+//      console.log("tog'")
+// }
+
+// let name = prompt("Ismingizni kiriting");
+// if(name.length > 5) {
+//      console.log("Salom" + " " + name )
+// }
+// else {
+//      console.log("hello" + " " + name)
+// }
+
+// let age = prompt("Enter your age");
+// age > 9 ? console.log("ikki xona") : console.log("bir xona")
+
+// console.log(6 == 6)  // true
+// console.log(6 == "6") // true
+// console.log(6 === "6") // false
+
+// let a = 5;
+// a = a + 5 defualt
+// a += 5 qisqartma
+
+// let sum = prompt("Enter your number");
+
+// for(let i = 0; i < 10; i++) {
+//      if(i % 2 === 0) {
+//           sum += i;
+//      }
+// }
+
+// console.log(sum);
+
+// let n = 75;
+
+// let one = n % 10;
+// let two = (n - one) / 10;
+// let result = one + two;
+
+// console.log(result);
+
+// let n = 75;
+
+// let one = n % 10;
+// let two = (n - one) / 10;
+// let result = one + "" + two ;
+
+// console.log(result);
+
+
+// let n = 530;
+
+// let one = (n % 100) / 100;
+
+// console.log(one);
+
+// let info = prompt("Enter your number");
+
+// let one = info % 10;
+// let two = ((info % 100)- one) / 10;
+// let three = (info - info % 100) / 100
+
+// let result = three + " " + two + " " + one;
+
+// console.log(result)
+
+
+// let info = prompt("Enter your number");
+
+// let one = info % 10;
+// let two = ((info % 100)- one) / 10;
+// let three = (info - info % 100) / 100
+
+// let result = (three + two + one);
+
+// console.log(result)
+
+
+// let info = prompt("Enter your number");
+
+// let one = info % 10;
+// let two = ((info % 100)- one) / 10;
+// let three = (info - info % 100) / 100
+
+// let result = one + " " + two + " " + three;
+
+// console.log(result)
+
+
+
+// let info = prompt("Enter your number");
+
+// let one = info % 10;
+// let two = ((info % 100)- one) / 10;
+// let three = (info - info % 100) / 100;
+
+// let result = two + " " + one + " " + three;
+
+// console.log(result)
