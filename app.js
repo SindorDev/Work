2534671379f629ce099dd1b3ab8578da25e33c85
Sindor007 <thesso003@gmail.com>
2024-04-29 12:20:08 +0500

    added function

--- app.js
+++ app.js
@@ -154,3 +154,258 @@
 // let result = two + " " + one + " " + three;
 
 // console.log(result)
+
+
+// let info = Number(prompt("Enter your number"));
+
+// if(info > 10) {
+//      console.log(info + 3);
+// }
+// else if (info < 10) {
+//      console.log(info * 2)
+// }
+// else {
+//      console.log(info + 12);
+// // }
+// setInterval(() => {
+//      document.body.innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} `
+// })
+
+// let country = prompt("Enter country");
+
+// switch(country) {
+//      case "Uzbekistan":
+//           console.log("Tashkent");
+//           break
+//      case "Kazakhstan": 
+//           console.log("Astana");
+//           break
+//      case "Tailwind":
+//           console.log("bali");
+// }
+
+// let age = prompt("Enter your age");
+
+// switch(true) {
+//      case age >= 10:
+//           console.log("boy");
+//           break;
+//      default:
+//           console.log("kid");
+// }
+
+// let age = prompt("Enter your age");
+
+// if (age > 10) {
+//      console.log("boy")
+// }
+// else{
+//      console.log("kid")
+// }
+
+
+// let age = prompt("Enter your age");
+
+// if (age > 10) {
+//      console.log("boy")
+// }
+// else{
+//      console.log("kid")
+// }
+
+// let age = prompt("Enter your age");
+
+// switch(age) {
+//      case "A":
+//           console.log("Mototsikl")
+//      break
+//      case "B":
+//           console.log("Yengil moshina")
+//      break
+     
+//      case "C":
+//           console.log("Yuk moshina")
+//      break
+     
+//      case "D":
+//           console.log("Avtobus moshina")
+//      break
+//      default:
+//           console.log("Bunday yonalish yoq")
+// }
+
+// let UserName = ["John", "Alex"];
+// let numbers = [3, 5, 2, 4, 5];
+
+// console.log(numbers[numbers.length - 1]);
+// let mixed = ["apple", 3, "peach", "true", "null"]
+
+// console.log(mixed[0]);
+// console.log(mixed[2]);
+// console.log(mixed[3]);
+
+// console.log(mixed.length)
+
+// let user = {
+//      firstname: "John",
+//      lastname: "Doe",
+//      age: 25,
+//      martialStatus:"married",
+//      aderess: {
+//           street: "52 avenue st.",
+//           home: 632
+//      },
+//      hobbies: ["music", "movies", "sports"]
+// }
+
+// console.log(user);
+// console.log(user.age);
+// console.log(user.lastname);
+// console.log(user.aderess.street);
+// console.log(user.hobbies[user.hobbies.length - 1]);
+
+// let users = [
+//      {
+//           name: "William",
+//           age: 20,
+//      },
+//      {
+//           name: "John",
+//           age: 25,
+//      }
+// ]
+
+// console.log(users[1].age)
+
+
+// let a = 4;
+// let b = 7;
+
+// if(a < b) {
+//      console.log("Katta son " + b)
+// }else if(a > b) {
+//      console.log("Katta son " + a)
+// }
+// else{
+//      console.log("Hato Son")
+// }
+
+// let a = 5;
+
+// if(a % 2 == 0) {
+//      console.log("Musbat Son")
+// }else {
+//      console.log("Manfiy Son")
+// }
+
+// let a = 7;
+
+// if(a % 2 == 0 ) {
+//      console.log("true")
+// }else {
+//      console.log("false")
+// }
+
+
+// let a = 14;
+
+// if(a % 4 == 0 || a % 3 == 0 ) {
+//      console.log("true")
+// }else {
+//      console.log("false")
+// }
+
+// let a = 2041;
+
+// if(a % 4 == 0) {
+//      console.log("Kabisa yili")
+// }
+// else {
+//      console.log("Kabisa yili emas")
+
+
+// let year = +prompt("yil kiriting");
+// let currentYear = new Date().getFullYear();
+
+// if(!isNaN(year)) {
+//      console.log(currentYear - year)
+// }
+// else {
+//      console.log("Yil kiriting")
+// }
+
+//  number with boolean
+
+// console.log(4 + true); // 5
+// console.log(4 + false); // 4
+// console.log(4 + true); // Infinity
+// console.log(4 + true); // -Infinity
+// console.log(5 * false); // 0
+
+
+// //  string with boolean
+
+// console.log("4" + true); // 4true
+// console.log("4" + false); // 3
+// console.log("4" + true); // 4
+// console.log(true - "2"); // -1
+// console.log("hello" - true); // Nan
+// console.log("hello" * false); // Nan
+
+
+// // string with undifined
+
+// console.log("hello" + undefined); // helloundefined
+// console.log("4" - undefined) //NAN
+
+// // boolean with undifined
+
+// console.log(true - undefined); // NaN
+// console.log(true + null) // 1
+// console.log(null / true) // 0
+// console.log(true / null) // infinity
+
+// //  string with null
+
+// console.log("hello" + null); // hellonull
+// console.log("4" + null) // 4null
+// console.log("4" - null) // 4
+
+// console.log(5 - ""); // 5
+// console.log(8 - " "); // 8
+
+// string to number
+// console.log(+"3")
+// console.log(Number("3"))
+// console.log(parseInt("3salom"))
+// console.log(parseFloat("3.5412salom"))
+
+
+// // number to string
+
+// console.log(String(3));
+// console.log((5).toString());
+// console.log(`${5}`);
+// console.log(10 + "");
+
+// // boolean
+
+// console.log(Boolean(0)); // false
+// console.log(Boolean(-1)); // true
+// console.log(Boolean(2)) // true
+// console.log(Boolean("")) // false
+// console.log(Boolean(" ")); // false
+// console.log(Boolean(undefined)) // false
+// console.log(Boolean({})); // true
+// console.log(Boolean([])); // true
+// console.log(Boolean(Symbol("salom"))); // true
+// console.log(Boolean(BigInt("salom"))); // true
+
+let start = +prompt("son kiriting ");
+let end = +prompt("son kiriting ");
+
+for(let i = start; i <= end; i++) {
+     if(i % 2 == 0) {
+          document.write( i )
+     }
+}
