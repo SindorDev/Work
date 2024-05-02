0d65273981379495e45c9a6a876c3307fd28292e
Sindor007 <thesso003@gmail.com>
2024-05-02 08:59:54 +0500

    added calculator

--- app.js
+++ app.js
@@ -643,3 +643,62 @@
 
 // } while (true);
 
+
+// let width = 30;
+// let height = 40;
+
+// function calculatoP(u, b ) {
+//      let p = (u + b) * 2;
+//      return p;
+// }
+
+// let res = calculatoP(width, height);
+
+// console.log(res)
+
+
+
+function calculator(n1, n2, option) {
+     switch(option) {
+          case "+": {
+               return n1 + n2;
+          }
+          case "-": {
+               return n1 - n2;
+          }
+          case "*": {
+               return n1 * n2;
+          }
+          case "/": {
+               return n1 / n2;
+          }
+          case "^": {
+               return Math.pow(n1, n2);
+          }
+          case "%": {
+               return n1 % n2;
+          }
+          default: {
+               console.log("Invalid option");
+          }
+     }
+}
+
+
+let  n1;
+while (typeof n1 !== "number") {
+     n1 = +prompt("Son kiriting");
+}
+
+let option;
+while (!["+", "-", "/", "*", "^", "%"].includes(option)) {
+     option = prompt("option: +, -, /, *, ^, % ");
+}
+
+let n2;
+while(typeof n2 !== "number") {
+     n2 = +prompt("Son kiriting");
+}
+
+let calc = calculator(n1, n2, option);
+console.log(calc);
